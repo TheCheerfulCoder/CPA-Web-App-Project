9bd6e3ed85949d726d08a3c38d826013b82f2fd4
JonasFP <jfieldpatton@gmail.com>
2021-12-24 16:39:44 -0800

    Finished creating the draft for the performance dashboard.

---
+++ performance.js
@@ -0,0 +1,30 @@
+const ctx = document.getElementById('myChart');
+
+
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6'],
+        datasets: [{
+            label: '% Score',
+            data: [25, 55, 45, 42, 52, 34, 50],
+            backgroundColor: [
+                'rgb(00, 80, 00)', 
+            ],
+            borderColor: [
+                'rgb(00, 80, 00)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+
+            }
+        }
+    }
+});
+
+
