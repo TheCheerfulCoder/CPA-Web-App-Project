a29184f7bad23a7e6ae2fff9f6fd5927222e8b37
JonasFP <jfieldpatton@gmail.com>
2022-01-24 23:03:02 -0800

    Finished getting the data set up for DOM manipulation to create the missed questions table.

--- performance.js
+++ performance.js
@@ -60,7 +60,12 @@ fetch("questions.json")
     })
     .then(function(data){
         loadedQuestions = data;
+        displayMissedQuestions();
     });
+
+displayMissedQuestions = () => {
+}
+
 
 /* Build a JavaScript Object for the table's data: I think key-value pairs
 should work well with this. The key will be the Question ID and the value 
@@ -69,9 +74,28 @@ the first ten or so letters with ellipsis.
 
 STEP 1: Get a count for each Question ID in the "missedQuestions" variable;
 store this in a key value pair with the Question ID as the key and the count
-as the value.
+as the value.*/
 
-STEP 2: Append the starting text of the question to the data structure from
+// Create an object that contains all the missed questions and their frequency.
+const missedQuestionsObject = {};
+for (const num of missedQuestions) {
+    missedQuestionsObject[num] = missedQuestionsObject[num] ? missedQuestionsObject[num] + 1 : 1;
+}
+console.log(missedQuestionsObject)
+
+// Turn that object into a multidemensional array.
+const missedQuestionsArray = Object.entries(missedQuestionsObject)
+console.log(missedQuestionsArray)
+
+/* NOTE: The following step may not be necessary. I just don't know if the
+   Object.entries() method sorts in descending order. */
+// Sort the multidemensional array in descending order based on frequency.
+missedQuestionsArray.sort(function (a, b) {
+    return b[1] - a[1];
+});
+console.log(missedQuestionsArray)
+
+/* STEP 2: Append the starting text of the question to the data structure from
 above.*/
 
 // Update the DOM to present the data: 
