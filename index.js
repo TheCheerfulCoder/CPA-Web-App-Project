c2ba1f37faf2caa3fd5cedfe907f474a3bb4b1ce
JonasFP <jfieldpatton@gmail.com>
2022-05-24 21:27:23 -0700

    Send user a reminder to email question ideas to itpracticetests@gmail.com.

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-var i = 0;
-var text = 'IT Practice Tests for BEC';
-var speed = 50;
+const testScores = window.localStorage.getItem('testScores');
+const matches = testScores.match(/\[.+?\]/g);
+const rawString = matches.join()
+const totalQuestionsAnswered = rawString.match(/0/g).length + rawString.match(/1/g).length
 
-function typeWriter() {
-    if (i < text.length) {
-        document.getElementById("header").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
+class Reminder {
+    display(msg) {
+        const modal = document.getElementById('modal');
+        document.getElementById('modal-text').innerHTML = msg;
+        const backDrop = document.getElementById('backdrop');
+        modal.classList.toggle('visible');
+        backDrop.classList.toggle('visible');
+        backDrop.addEventListener(
+            'click', () => {
+                modal.classList.toggle('visible');
+                backDrop.classList.toggle('visible');
+            }
+        );
     }
 }
 
-typeWriter();
+const emailQuestionsReminder = new Reminder();
+
+
+switch (totalQuestionsAnswered) {
+    case 20:
+        emailQuestionsReminder.display(
+            "Congradulations on completing 20 questions!🥳 Please send questions to itpracticetests@gmail.com"
+        );
+        break;
+    case 40:
+        emailQuestionsReminder.display(
+            "Congradulations on completing 40 questions!🥳 Please send questions to itpracticetests@gmail.com"
+        );
+        break;
+    case 80:
+        emailQuestionsReminder.display(
+            "Congradulations on completing 80 questions!🥳 Please send questions to itpracticetests@gmail.com"
+        );
+}
+
+// var i = 0;
+// var text = 'IT Practice Tests for BEC';
+// var speed = 50;
+
+// function typeWriter() {
+//     if (i < text.length) {
+//         document.getElementById("header").innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+// typeWriter();
