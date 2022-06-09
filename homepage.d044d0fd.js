648f7b6134be30473ef8e1b74ca09b7d17086c47
JonasFP <jfieldpatton@gmail.com>
2022-06-09 10:55:42 -0700

    Remove styled scroll bar for mobile devices.

--- homepage.d044d0fd.js
+++ homepage.d044d0fd.js
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49558" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50196" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
