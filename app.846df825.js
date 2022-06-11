1269369de592c2d2281296bb9c4404ea906e683d
JonasFP <jfieldpatton@gmail.com>
2022-06-11 15:07:35 -0700

    Testing ways to make navigation intuitive.

--- app.846df825.js
+++ app.846df825.js
@@ -119,7 +119,8 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"shared/app.js":[function(require,module,exports) {
 /* Make navigation within the app intuitive for optimal user experiance. */
-history.pushState(null, '', '/CPA-Web-App-Project/index.html'); // addEventListener('popstate', event => { location.reload() });
+// history.pushState(null, '', '/CPA-Web-App-Project/index.html');
+// addEventListener('popstate', event => { location.reload() });
 },{}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -148,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53617" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54164" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
